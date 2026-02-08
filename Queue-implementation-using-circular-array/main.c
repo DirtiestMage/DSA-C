8b0d5aabb4856a7635f4a83970c90376ab3a9b3c
Aditya Jha <78aaditya@gmail.com>
2026-02-08 20:15:18 +0530

    queue

---
+++ Queue-implementation-using-circular-array/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#define MAX_SIZE 10
+
+typedef struct que{
+    
+    int queue[MAX_SIZE];
+    int front;
+    int rear;
+    
+}queue;
+
+int main() {
+    
+    return 0;
+}
